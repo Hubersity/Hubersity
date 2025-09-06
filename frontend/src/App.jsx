ffba4d6b65bc7b699a3dc783c28b0f3b35d38acf
karnponpoochitkanon <karnpon.p@ku.th>
2025-09-06 14:04:32 +0700

    feat: setup homepage with Tailwind + layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
+import Home from './pages/Home';
+
 function App() {
   return (
-    <div className="text-center mt-10">
-      <h1 className="text-4xl font-bold text-green-700">
-        🎉 Hello Hubersity!
-      </h1>
-      <p className="mt-4 text-gray-600">Tailwind CSS is working ✅</p>
+    <div>
+      <Home />
+      {/* <Login /> Aong ja ma tum */}
     </div>
   );
 }
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+
+function Home() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold text-green-700">
+        Hello Hubersity!
+      </h1>
+      <p className="mt-4 text-gray-600">Tailwind CSS is working ✅</p>
+    </div>
+  );
+}
+
+export default Home;
