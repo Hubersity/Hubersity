bf3631f990759ccd0b0ae4c54d5706a389d31e53
karnponpoochitkanon <karnpon.p@ku.th>
2025-09-06 13:06:10 +0700

    feat: setup Tailwind and create Home page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold text-green-700">
+        🎉 Hello Hubersity!
+      </h1>
+      <p className="mt-4 text-gray-600">Tailwind CSS is working ✅</p>
+    </div>
+  );
+}
+
+export default App;
