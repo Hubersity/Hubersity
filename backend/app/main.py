639422f5f0d5d533cddfbcb4b340c8680e5b03b5
KhittitajB <khittitaj.b@ku.th>
2025-10-04 19:50:58 +0700

    Added study calendar router

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import text
 from . import models
 from .database import engine
-from .routers import users, auth
+from .routers import users, auth, study_calendar
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -33,6 +33,7 @@ except Exception as e:
 
 app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(study_calendar.router)
 
 @app.get("/")
 def root():
