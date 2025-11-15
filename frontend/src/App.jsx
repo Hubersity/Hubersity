15d683f7000af5f3fcc980887115d260c99a7010
karnponpoochitkanon <karnpon.p@ku.th>
2025-11-15 11:26:34 +0700

    add App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -83,6 +83,8 @@ const Report              = lazy(() => import('./pages/Admin/Report.jsx'));
 const Notification_admin  = lazy(() => import('./pages/Admin/Notification-admin.jsx'));
 const PostDetail          = lazy(() => import('./pages/Admin/PostDetail.jsx'));
 const UserDetail          = lazy(() => import('./pages/Admin/UserDetail.jsx'));
+const News_admin          = lazy(() => import('./pages/Admin/News-admin.jsx'));
+const NewsCreate_admin    = lazy(() => import('./pages/Admin/NewsCreate-admin.jsx'));
 
 // Simple Error Boundary to isolate admin crashes
 class AdminErrorBoundary extends React.Component {
@@ -149,6 +151,8 @@ export default function App() {
         <Route path="report/user/:username" element={<Suspense fallback={null}><UserDetail /></Suspense>} />
         {/* <Route path="tags-admin" element={<Suspense fallback={null}><Tags /></Suspense>} /> */}
         <Route path="noti-admin" element={<Suspense fallback={null}><Notification_admin /></Suspense>} />
+        <Route path="news" element={<Suspense fallback={null}><News_admin /></Suspense>} />
+        <Route path="news/create" element={<Suspense fallback={null}><NewsCreate_admin /></Suspense>} />
       </Route>
 
       {/* fallback */}
